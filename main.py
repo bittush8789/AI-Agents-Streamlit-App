25354a2e35c6042dd54dac9a89f4c8ffdbdccd25
Bittu sharma <bittush9534@gmail.com>
2025-02-10 10:41:03 +0530

    added main.py file

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from docx import Document
 from io import BytesIO
 import base64
 
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 load_dotenv()
 
 # LLM object and API Key
